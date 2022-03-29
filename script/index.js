5d96bc12aaa38048aeb942d06548c37bee2d22b9
Ekaterina Ivannikova <katrina-iv75@mail.ru>
2022-03-29 22:14:50 +0300

    practic4

---
+++ script/index.js
@@ -0,0 +1,42 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let saveButton = document.querySelector('.popup__save-button');
+let closeButton = document.querySelector('.popup__close_icon');
+let formElement = document.querySelector('.popup-form');
+let inputName = formElement.querySelector('.popup-input__name');
+let inputJob = formElement.querySelector('.popup-input__job');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let elementLike = document.querySelectorAll('.element__like');
+
+inputName.value = profileTitle.textContent;
+inputJob.value =  profileSubtitle.textContent;
+
+document.querySelectorAll('.element__like').forEach(item => {
+    item.addEventListener('click', event => {
+        item.classList.toggle('element__like_active');
+    });
+  });
+
+function togglePopup() {
+    popup.classList.toggle('popup_opened');
+}
+
+editButton.addEventListener('click', togglePopup);
+saveButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+
+function onOverlayClick(event) {
+    if (event.target === event.currentTarget) {
+        togglePopup();
+    }
+}
+popup.addEventListener('click', onOverlayClick);
+
+function saveInput(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = inputName.value;
+    profileSubtitle.textContent = inputJob.value;
+}
+
+formElement.addEventListener('submit', saveInput);
